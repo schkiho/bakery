7fd4a501845708467c8a0b7cd1972c530b0beb8f
schkiho <schkiho@yahoo.de>
2019-12-03 15:49:12 +0100

    setup controller functions tested backend with postman

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Order;
+use App\Http\Resources\Order as OrderResource;
 
 class OrdersController extends Controller
 {
--- app/Http/Resources/Order.php
+++ app/Http/Resources/Order.php
@@ -14,6 +14,9 @@ class Order extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'created_at' => $this->created_at
+        ];
     }
 }
